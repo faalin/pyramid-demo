a98f93e1903c52f37a1879c62f508d3cf0960090
fanlin <fanlin@wolongdata.com>
2017-09-11 15:39:42 +0800

    main

--- demo.py
+++ demo.py
@@ -5,14 +5,20 @@ from pyramid.response import Response
 
 def hello_world(request):
     print(request.matchdict)
-    return Response('Hello World! %(name)s CHINA' % request.matchdict)
+    return Response('Hello World!')
+
+
+def main(**settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.add_route('hello', '/')
+    config.add_view(hello_world, route_name='hello')
+    config.scan()
+    return config.make_wsgi_app()
+
 
 if __name__ == '__main__':
-    with Configurator() as config:
-        config.include("pyramid_debugtoolbar")
-        config.add_route('hello', '/test/{name}')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-
+    app = main()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
